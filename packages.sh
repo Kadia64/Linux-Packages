0628f8b5d8eb0e7f507edf546c066cdadbb29509
Kadia64 <84291422+Kadia64@users.noreply.github.com>
2025-08-20 12:15:43 -0500

    initial commit

---
+++ packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+echo "installing net tools:"
+sudo apt install net-tools
+
+echo "installing vscode:"
+sudo snap install --classic code
+
+echo "installing git:"
+sudo apt install git
+
+echo "installing curl:"
+sudo apt install curl
+
+echo "installing openssh-server:"
+sudo apt install openssh-server openssh-client
+
+echo "installing docker:"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+echo "installing npm:"
+sudo apt install npm
+
+echo "installing nodejs:"
+sudo apt install nodejs
+
+
